1355f8be14fd8b1f26c0af54326a76ee59504770
Christian <christiantenz7@gmail.com>
2024-05-26 21:51:51 -0500

    dto added

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,1 +1,35 @@
-export class CreateProductDto {}
+import {
+  IsArray,
+  IsIn,
+  IsInt,
+  IsNumber,
+  IsOptional,
+  IsPositive,
+  IsString,
+  MinLength,
+} from 'class-validator';
+
+export class CreateProductDto {
+  @IsString()
+  @MinLength(3)
+  readonly title: string;
+  @IsNumber()
+  @IsPositive()
+  @IsOptional()
+  readonly price: number;
+  @IsOptional()
+  @IsString()
+  readonly description: string;
+  @IsString()
+  @IsOptional()
+  readonly slug: string;
+  @IsInt()
+  @IsPositive()
+  @IsOptional()
+  readonly stock: number;
+  @IsString({ each: true })
+  @IsArray()
+  readonly sizes: string[];
+  @IsIn(['men', 'women', 'unisex'])
+  readonly gender: string;
+}
--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -1,46 +1,40 @@
-import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
 export class Product {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
 
-    @PrimaryGeneratedColumn('uuid')
-    id:string;
+  @Column('text', {
+    unique: true,
+  })
+  title: string;
 
-    @Column('text', {
-        unique: true
-    })
-    title:string;
+  @Column('numeric', {
+    default: 0,
+  })
+  price: number;
 
-    @Column('numeric',{
-        default: 0
-    })
-    price:number;
+  @Column('text', {
+    nullable: true,
+  })
+  description: string;
 
-    @Column('text',{
-        nullable:true
-    })
-    description: string;
+  @Column('text', {
+    unique: true,
+  })
+  slug: string;
 
-    @Column('text', {
-        unique:true
-    })
-    slug:string;
+  @Column('int', {
+    default: 0,
+  })
+  stock: number;
 
-    @Column('int',
-        {
-            default: 0
-        }
-    )
-    stock:number;
+  @Column('text', {
+    array: true,
+  })
+  sizes: string[];
 
-    @Column('text',
-        {
-            array:true
-        }
-    )
-    sizes:string[]
-
-    @Column('text')
-    gender:string;
-
+  @Column('text')
+  gender: string;
 }
