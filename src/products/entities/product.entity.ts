0febaa620e91d5cc23e0a6bb09b3738d53ab027a
Christian <christiantenz7@gmail.com>
2024-05-27 21:24:09 -0500

    .

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -1,29 +1,34 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { BeforeInsert, Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
 export class Product {
   @PrimaryGeneratedColumn('uuid')
   id: string;
 
+  //TODO: Obligatorio mandar
   @Column('text', {
     unique: true,
   })
   title: string;
 
-  @Column('numeric', {
+  //TODO: Si no lo envia por defecto el precio sera cero
+  @Column('float', {
     default: 0,
   })
   price: number;
+  //TODO: La descripcion puede ser null
 
   @Column('text', {
     nullable: true,
   })
-  description: string;
+  description?: string;
+  
+  //TODO: El slug no es necesario que lo envie, ya que se genera desde el nombre del producto
 
   @Column('text', {
     unique: true,
   })
-  slug: string;
+  slug?: string;
 
   @Column('int', {
     default: 0,
@@ -37,4 +42,17 @@ export class Product {
 
   @Column('text')
   gender: string;
+
+  //Antes de insertar quiero crear el slug en base al nombre del producto
+  @BeforeInsert()
+  checkSlugInsert() {
+    if (!this.slug) {
+      this.slug === this.title;
+    }
+
+    this.slug = this.slug
+      .toLowerCase()
+      .replaceAll(' ', '_')
+      .replaceAll("'", '');
+  }
 }
--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,25 +1,46 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Product } from './entities/product.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class ProductsService {
-  create(createProductDto: CreateProductDto) {
-    return 'This action adds a new product';
-  }
+  constructor(
+    @InjectRepository(Product)
+    private readonly productRepository: Repository<Product>,
+  ) {}
 
-  findAll() {
-    return `This action returns all products`;
-  }
+  //* CREAR
+  async create(createProductDto: CreateProductDto) {
+    try {
+      const product = this.productRepository.create(createProductDto);
 
+      const productSaved = await this.productRepository.save(product);
+
+      return productSaved;
+    } catch (error) {
+      throw new InternalServerErrorException('Ayuda');
+    }
+  }
+  //* TRAER TODOS LOS REGISTROS
+  async findAll() {
+      try {
+        const products = this.productRepository.find();
+      } catch (error) {
+        
+      }
+  }
+  //* TRAER UN REGISTRO
   findOne(id: number) {
     return `This action returns a #${id} product`;
   }
-
+  //* ACTUALIZAR
   update(id: number, updateProductDto: UpdateProductDto) {
     return `This action updates a #${id} product`;
   }
-
+  //* ELIMINAR
   remove(id: number) {
     return `This action removes a #${id} product`;
   }
